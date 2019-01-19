c6eca310737f37c54b9777a368e1733db999b81f
Aree Vanier <gregory@ajayinkingston.com>
2019-01-19 10:54:32 -0500

    Gutted OI, removed assignments from 2018 bot

--- src/main/java/frc/robot/OI.java
+++ src/main/java/frc/robot/OI.java
@@ -29,63 +29,20 @@ public class OI {
 	// button number definitions
 	
 	// right joystick buttons
-	final int STRAIGHT_GYRO = 1;
-	final int ARCADE_GYRO = 2;
-	final int CANCEL_AUTO = 2;
+	// -- final int BUTTON_NAME = number;
 
 	// left joystick buttons
-	final int RESET_ODOM = 2;
-	final int DROP_OVERRIDE = 1;
-
-	// xbox
-	// final int SCALE_HEIGHT = XBoxController.XBOX_Y;
-	// final int SCALE_LOWER_HEIGHT = XBoxController.XBOX_RB;
-	// final int SWITCH_HEIGHT = XBoxController.XBOX_X;
-	// final int GROUND_HEIGHT = XBoxController.XBOX_A;
-	// final int TOGGLE_CLAW_POSITION = XBoxController.XBOX_B;
-	// final int EJECT = XBoxController.XBOX_DPAD_UP_ANGLE;
-	// final int RESET_LIFT_POSITION = XBoxController.XBOX_START;
-	// final int HOLD_CLIMB_VOLTAGE = XBoxController.XBOX_BACK;
-	// final int TOGGLE_INTAKE_OPEN = XBoxController.XBOX_LB;
-	// final int SET_INTAKE_OPEN = XBoxController.XBOX_DPAD_LEFT_ANGLE;
-	// final int SET_INTAKE_CLOSE = XBoxController.XBOX_DPAD_RIGHT_ANGLE;
 
 	// controller initialization
 	public Joystick l_joy = new Joystick(L_JOY);
 	public Joystick r_joy = new Joystick(R_JOY);
 	public XBoxController xbox = new XBoxController(XBOX);
 
-	// button initialization
-	public Button straight_gyro_button = new JoystickButton(r_joy, STRAIGHT_GYRO);
-	public Button arcade_gyro_button = new JoystickButton(r_joy, ARCADE_GYRO);
-	public Button cancel_auto_button = new JoystickButton(l_joy, CANCEL_AUTO);
-	public Button drop_override = new JoystickButton(l_joy, DROP_OVERRIDE);
-
-	// public XBoxButton eject_button = new XBoxButton(xbox, EJECT);
-	// public XBoxTrigger intake_trigger = new XBoxTrigger(xbox, false);
-	// public XBoxTrigger outtake_trigger = new XBoxTrigger(xbox, true);
-	// public XBoxButton scale_height_button = new XBoxButton(xbox, SCALE_HEIGHT);
-	// public XBoxButton scale_lower_height_button = new XBoxButton(xbox, SCALE_LOWER_HEIGHT);
-	// public XBoxButton switch_height_button = new XBoxButton(xbox, SWITCH_HEIGHT);
-	// public XBoxButton ground_height_button = new XBoxButton(xbox, GROUND_HEIGHT);
-	// public XBoxButton hold_climb_button = new XBoxButton(xbox, HOLD_CLIMB_VOLTAGE);
-	// public XBoxButton toggle_claw_position_button = new XBoxButton(xbox, TOGGLE_CLAW_POSITION);
-	// public XBoxButton reset_lift_position = new XBoxButton(xbox, RESET_LIFT_POSITION);
-	// public XBoxButton toggle_intake_open = new XBoxButton(xbox, TOGGLE_INTAKE_OPEN);
-	// public XBoxPOVButton set_intake_open = new XBoxPOVButton(xbox, SET_INTAKE_OPEN);
-	// public XBoxPOVButton set_intake_close = new XBoxPOVButton(xbox, SET_INTAKE_CLOSE);
+	// button initialization 
+	// -- public Button buttonName = new JoystickButton(controller, BUTTON_NAME);
 
 	public void init() {
-		straight_gyro_button.whileHeld(new StraightGyroDriveCommand());
-		arcade_gyro_button.whileHeld(new ArcadeGyroDriveCommand());
-		// toggle_claw_position_button.toggleWhenPressed(new ToggleClawPositionCommand());
-		// reset_lift_position.whenPressed(new ResetLiftPositionCommand());
-		// toggle_intake_open.whenPressed(new IntakeOpenCommand());
-		// set_intake_open.whenPressed(new IntakeOpenCommand(true));
-		// set_intake_close.whenPressed(new IntakeOpenCommand(false));
-		
-//		CancelAuto cancel_auto = new CancelAuto();
-//		cancel_auto.setRunWhenDisabled(true);
-//		cancel_auto_button.whenPressed(cancel_auto);
+		//Inithalise event handling
+		// -- buttonName.whenHeld(new Command())
 	}
 }
